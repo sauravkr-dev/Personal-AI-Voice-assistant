2cce634a4532e3c6e83c67fcfdc2bb7dee146f69
Saurav Kumar <224896960+sauravkr-dev@users.noreply.github.com>
2026-07-04 18:02:20 +0530

    Enhance Nova AI with language support and commands
    Refactor Nova AI assistant to support language translation, desktop commands, and improved audio handling.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-
 import speech_recognition as sr
 import webbrowser
-import pyttsx3
 import requests
-import msvcrt
 import musiclibrary
 import sys
 from gtts import gTTS
@@ -15,12 +12,27 @@ import uuid
 from websites import websites
 from greetings import greetings
 import random
+import pyautogui
+import msvcrt
+from languages import languages
+from deep_translator import GoogleTranslator
 
+current_language = "en-IN"
 wake_words = ["nova", "nov", "innova"]
+ai_access = False
+minimized = False
+
+
+def translate_to_english(text):
+    try:
+        return GoogleTranslator(
+            source="auto",
+            target="en"
+        ).translate(text)
+    except:
+        return text
 
 def aiProcess(command):
-    with open("gemini_api.txt", "r") as f:
-        gemini_api = f.read().strip()
 
     client = genai.Client(api_key=gemini_api)
 
@@ -39,11 +51,8 @@ Always introduce yourself as Nova.
 
     return response.text
 
-with open("news_api.txt", "r") as f:
-        news_api = f.read().strip()
 
 recognizer = sr.Recognizer()
-engine = pyttsx3.init()
 
 def is_connected():
     try:
@@ -52,15 +61,37 @@ def is_connected():
     except:
         return False
 
-def speak_old(text):
-    engine.say(text)
-    engine.runAndWait()
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.dirname(os.path.abspath(__file__))
+
+    return os.path.join(base_path, relative_path)
+
+with open(resource_path("gemini_api.txt"), "r") as f:
+    gemini_api = f.read().strip()
+
+with open(resource_path("news_api.txt"), "r") as f:
+    news_api = f.read().strip()
 
 pygame.mixer.init()
+pygame.mixer.init()
+
 def speak(text):
     filename = f"{uuid.uuid4()}.mp3"
 
-    tts = gTTS(text, lang="en")
+    if current_language != "en-IN":
+        text = GoogleTranslator(
+            source="en",
+            target=current_language.split("-")[0]
+        ).translate(text)
+
+    tts = gTTS(
+        text=text,
+        lang=current_language.split("-")[0]
+    )
+
     tts.save(filename)
 
     pygame.mixer.music.load(filename)
@@ -73,14 +104,12 @@ def speak(text):
     pygame.mixer.music.unload()
 
     time.sleep(0.1)
+
     if os.path.exists(filename):
         os.remove(filename)
 
-if not os.path.exists("yes.mp3"):
-    gTTS(text="Yes", lang="en").save("yes.mp3")
-
 def play_audio(filename):
-    pygame.mixer.music.load(filename)
+    pygame.mixer.music.load(resource_path(filename))
     pygame.mixer.music.play()
 
     while pygame.mixer.music.get_busy():
@@ -91,13 +120,20 @@ def play_audio(filename):
     time.sleep(0.05)
 
 def processCommand(c):
-    global active
+    global active, ai_access, minimized, current_language
     #print("processCommand:", repr(c))
     print(f"Command: {c}")
 
+    if "language" in c.lower():
+        for name, code in languages.items():
+            if name in c.lower():
+                current_language = code
+                print(f"Language changed to {name.title()}.")
+                speak(f"Language changed to {name}.")
+                return
 
     if "open" in c.lower():
-        
+
         search_query = c.lower().replace("open", "").strip()
         site = search_query.replace(" ", "").replace(".com", "").replace(".in", "").strip()
         if site in websites:
@@ -127,7 +163,7 @@ def processCommand(c):
         webbrowser.open(url)
         print(f"Playing {name}")
         speak(f"Playing {name}")
-
+        
     elif "play" in c.lower():
         if not is_connected():
             print("No internet connection.")
@@ -144,14 +180,13 @@ def processCommand(c):
         webbrowser.open(f"https://open.spotify.com/search/{song.replace(' ', '%20')}")
         print(f"searching {song} on spotify")
         speak(f"searching {song} on spotify")
-        #speak("Sorry, I couldn't find that song.")
+
 
     elif "news" in c.lower():
         if not is_connected():
             print("No internet connection.")
             speak("No internet connection.")
             return
-        
         response = requests.get(
             f"https://newsapi.org/v2/everything?q=india&language=en&sortBy=publishedAt&apiKey={news_api}",
             timeout=10
@@ -169,10 +204,13 @@ def processCommand(c):
             i = 0
             print("Press 'q' to stop the news...")
             speak("Press 'q' to stop the news...")
+            print("Here are today's top headlines.")
+            speak("Here are today's top headlines.")
             while i < len(articles):
 
                 #listen 5 news
                 for _ in range(5):
+                    key = ""
                     if msvcrt.kbhit():
                         key = msvcrt.getch().decode(errors="ignore").lower()
 
@@ -219,8 +257,8 @@ def processCommand(c):
                         speak("Okay, stopping the news.")
                         break
 
-                    # Disable Nova
-                    elif any(x in reply for x in ["exit", "quit", "stop", "deactivate", "sleep",]):
+                    # deactivate Nova
+                    elif any(x in reply for x in ["deactivate yourself", "go to sleep", "deactivate nova", "sleep nova", "nova sleep", "nova deactivate"]):
                         print("Deactivating Nova...")
                         speak("Deactivating Nova...")
                         active = False
@@ -240,29 +278,195 @@ def processCommand(c):
                     print("I didn't understand. Stopping the news.")
                     speak("I didn't understand. Stopping the news.")
                     break
+    if "desktop" in c.lower() and any(x in c.lower() for x in [
+    "refresh",
+    "reload"
+]):
+        print("Refreshing desktop.")
+        speak("Refreshing desktop.")
+        time.sleep(0.5)
+        minimized = True
+        pyautogui.hotkey("win", "d")   
+        time.sleep(0.8)
+        pyautogui.press("f5")
 
-    elif any(word in c.lower() for word in ["close", "shutdown", "goodbye"]):
-        print("Goodbye!")
-        speak("Goodbye!")
+    elif any(x in c.lower() for x in [
+    "refresh",
+    "reload",
+]):
+        print("Refreshing.")
+        speak("Refreshing.")
+        time.sleep(0.5)
+        pyautogui.press("f5")
+
+    elif any(x in c.lower() for x in [
+    "show desktop",
+    "go to desktop",
+    "desktop mode",
+    "minimize all windows",
+    "minimize windows",
+    "minimise all windows",
+    "minimise windows"
+]):
+        if minimized:
+            print("Desktop is already showing.")
+            speak("Desktop is already showing.")
+        else:
+            print("Showing desktop.")
+            speak("Showing desktop.")
+            time.sleep(0.5)
+            pyautogui.hotkey("win", "d")
+            minimized = True
+
+    elif( 
+    any(word in c.lower() for word in ["window", "windows"])
+    and
+    any(x in c.lower() for x in [
+        "restore",
+        "show",
+        "bring back"
+    ])
+):
+        if minimized:
+            print("Restoring all windows.")
+            speak("Restoring all windows.")
+            time.sleep(0.5)
+            pyautogui.hotkey("win", "d")
+            minimized = False
+        else:
+            print("Windows are already open.")
+            speak("Windows are already open.")
+
+    elif any(x in c.lower() for x in [
+    "close window",
+    "close current window",
+    "close this window",
+    "close the window"
+]):
+        print("Closing current window.")
+        speak("Closing current window.")
+        time.sleep(0.5)      #To complete TTS
+        pyautogui.hotkey("alt", "f4")
+
+    elif "close" in c.lower() and any(x in c.lower() for x in [
+    "all windows",
+    "everything",
+    "apps",
+    "applications"
+    ]):
+        print("Are you sure you want to close all windows?")
+        speak("Are you sure you want to close all windows?")
+
+        with sr.Microphone() as source:
+            audio = recognizer.listen(source, timeout=5, phrase_time_limit=3)
+
+        reply = recognizer.recognize_google(audio).lower()
+        print(f"Heard: {reply}")
+
+        if any(x in reply for x in ["yes", "sure", "go on", "ofcourse"]):
+            print("Closing all windows.")
+            speak("Closing all windows.")
+            for _ in range(20):
+                pyautogui.hotkey("alt", "f4")
+                time.sleep(0.5)
+        elif any(x in reply for x in ["no", "stop"]):
+            print("Cancelled.")
+            speak("Cancelled.")
+
+
+    elif any(x in c.lower() for x in [
+    "shutdown the pc",
+    "shutdown pc",
+    "shutdown computer",
+    "turn off the pc",
+    "power off",
+    "turn off the computer",
+    "turn off my pc",
+    "turn off my computer",
+    ]):
+        print("Your computer will shut down in 10 seconds.")
+        speak("Your computer will shut down in 10 seconds.")
+        os.system("shutdown /s /t 10")
+
+    elif any (x in c.lower() for x in [
+        "restart the pc", "restart pc", "restart computer", 
+        "reboot the pc", 
+        "reboot pc", 
+        "reboot computer"
+        "restart the computer",
+        "reboot the computer",
+        "restart my pc",
+        "restart my computer"
+        ]):
+        print("Your computer will restart in 10 seconds.")
+        speak("Your computer will restart in 10 seconds.")
+        os.system("shutdown /r /t 10")
+        
+    elif any(x in c.lower() for x in [
+        "cancel shutdown",
+        "cancel restart",
+        "abort shutdown",
+        "abort restart",
+        "stop shutdown",
+        "stop restart",
+        "cancel the shutdown",
+        "cancel the restart",
+        "abort the shutdown",
+        "abort the restart"
+        ]):
+        result = os.system("shutdown /a")
+
+        if result == 0:
+            print("Shutdown or restart cancelled.")
+            speak("Shutdown or restart cancelled.")
+        else:
+            print("No shutdown or restart was scheduled.")
+            speak("No shutdown or restart was scheduled.")
+        
+
+    elif any(x in c.lower() for x in ["ai access", "enable ai", "enable ai access", "enable ai mode", "ai mode"]):
+        ai_access = True
+        print("AI access enabled.")
+        speak("AI access enabled.")
+
+    elif any(x in c.lower() for x in ["disable ai", "disable ai access", "disable ai mode"]):
+        ai_access = False
+        print("AI access disabled.")
+        speak("AI access disabled.")
+
+    elif any(word in c.lower() for word in ["goodbye", "disable", "shutdown", "shutdown yourself", "disable yourself", "close yourself"]):
+        print("Disabling Nova...")
+        speak("Disabling Nova...")
         sys.exit()
-        #other commands will attend by gemini
+        #let gemini handle the command if ai_access is enabled
     else:
-        if not is_connected():
-            print("No internet connection.")
-            speak("No internet connection.")
-            return
-        response = aiProcess(c)
-        print(response)
-        speak(response)
+        if ai_access:
+            if not is_connected():
+                print("No internet connection.")
+                speak("No internet connection.")
+                return
+            response = aiProcess(c)
+            print(response)
+            speak(response)
 
-
+print("=" * 41)
+print("      NOVA AI VOICE ASSISTANT")
+print("=" * 41)
+print("Status    : Running")
+print("Wake Word : Nova")
+print("Ready to receive voice commands...")
+print("=" * 41)
+print()
     
 
 if __name__ == "__main__":
+    print("Initializing Nova...")
     speak("Initializing Nova...")
 
     with sr.Microphone() as source:
         recognizer.adjust_for_ambient_noise(source, duration=1)
+        recognizer.pause_threshold = 1.2
+        recognizer.non_speaking_duration = 0.6
     active = False
     while True:
         #Listen for the wake word "Nova"
@@ -272,13 +476,25 @@ if __name__ == "__main__":
             with sr.Microphone() as source:
                 if active:
                     print("Listening...")
-                    audio = recognizer.listen(source, timeout=None, phrase_time_limit=5)
+                    audio = recognizer.listen(source, timeout=None, phrase_time_limit=7)
                 else:
                     print("Listening for wake word...")
-                    audio = recognizer.listen(source, timeout=None, phrase_time_limit=3)
+                    audio = recognizer.listen(source, timeout=None, phrase_time_limit=8)
 
-            text = recognizer.recognize_google(audio).lower()
+            text = recognizer.recognize_google(
+                audio,
+                language=current_language
+            ).lower()
+
             print(f"Heard: {text}")
+
+            original_text = text
+
+            if current_language != "en-IN":
+                text = translate_to_english(text).lower()
+
+            print("Original:", original_text)
+            print("Command :", text)
 
             if not active:
                 if any(word in text for word in wake_words):
@@ -314,7 +530,7 @@ if __name__ == "__main__":
                 if command in greetings:
                     play_audio(greetings[command])
 
-                elif command in ["exit", "quit", "stop", "deactivate", "sleep",]:
+                elif command in ["deactivate yourself", "go to sleep", "deactivate", "sleep",]:
                     active = False
                     print("Deactivating Nova...")
                     speak("Deactivating Nova...")
@@ -325,7 +541,7 @@ if __name__ == "__main__":
                     play_audio(greetings[command])                
                 continue
 
-            if text in ["exit", "quit", "stop", "deactivate", "sleep",]:
+            if text in ["deactivate yourself", "go to sleep", "deactivate", "sleep",]:
                 active = False
                 print("Deactivating Nova...")
                 speak("Deactivating Nova...")
