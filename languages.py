fc407a2f0e391a92b0a0b2ad77dc9e4686af4734
Saurav Kumar <224896960+sauravkr-dev@users.noreply.github.com>
2026-07-04 18:02:58 +0530

    Add language mappings to languages.py

---
+++ languages.py
@@ -0,0 +1,16 @@
+languages = {
+    "english": "en-IN",
+    "inglish": "en-IN",
+
+    "hindi": "hi-IN",
+    "हिंदी": "hi-IN",
+
+    "japanese": "ja-JP",
+    "japan": "ja-JP",
+
+    "spanish": "es-ES",
+    "español": "es-ES",
+
+    "french": "fr-FR",
+    "français": "fr-FR"
+}
